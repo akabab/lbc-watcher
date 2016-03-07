26e6e698a19dc231ae66a0a932d2ad9fdf94d13e
akabab <ycribier@student.42.fr>
2016-03-07 18:37:26 +0100

    ~

--- server.js
+++ server.js
@@ -2,5 +2,10 @@
 var Scrapper = require('./lib/Scrapper');
 var config = require('./config.js');
 
-Scrapper.recoverOffers();
-setInterval(Scrapper.recoverOffers, config.scrapper.delay * 60 * 1000);
+var urls = config.scrapper && config.scrapper.urls;
+
+urls.forEach(function (url) {
+	Scrapper.recoverOffers(url);
+	setInterval(function () { Scrapper.recoverOffers(url); }, config.scrapper.delay * 60 * 1000);
+});
+
