a712b8847dd066e29c32916970816cb313aa731c
Yoann Cribier <yoann.cribier@openjet.com>
2016-06-15 14:40:03 +0200

    default config

---
+++ default-config.js
@@ -0,0 +1,19 @@
+// Create a config.js with your personnal configuration
+// Then 'npm start' command
+
+module.exports = {
+    "scrapper": {
+        "active": true,
+        "delay": 2, // min
+        "outputFile": "offers.json",
+        "urls": [
+            "https://www.leboncoin.fr/...", // url you want to match results
+        ]
+    },
+    "sms": [
+        { "send": true, "user": "...", "pass": "" } // 'Free' users only
+    ],
+    "emails": [
+        "user@exemple.com"
+    ]
+}
