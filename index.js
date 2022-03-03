2a06da4b5c42b6e8e0a38468ac7688e65aad4e67
Yoann Cribier <yoann_cribier@hotmail.com>
2022-03-03 11:30:37 +0400

    refactor

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const { recoverOffers } = require('./lib/scrapper')
+const Scrapper = require('./lib/scrapper')
 const config = require('./config.js')
+
+const TelegramBot = require('node-telegram-bot-api');
 
 const fs = require('fs')
 const path = require('path')
-const moment = require('moment')
 
 const Sms = require('./lib/sms')
 const Mail = require('./lib/mail')
+
+// Create a bot that uses 'polling' to fetch new updates
+const { token, chatId } = config.telegram
+const Bot = new TelegramBot(token, { polling: true });
+
+const urls = config.scrapper && config.scrapper.urls
+
+// == HELPERS ==
+
+const wait = ms => new Promise(r => setTimeout(r, ms));
+const flatMap = arr => arr.reduce((acc, e) => [ ...acc, ...e ], [])
+//
+
 
 let OFFERS = {}
 
@@ -17,51 +31,87 @@ try {
   console.error(e)
 }
 
-const flatMap = arr => arr.reduce((acc, e) => [ ...acc, ...e ], [])
 
-const prepareMailHtml = offers => {
-  return offers.map(offer => `
-    <div className='offer' style='background-color: gold; margin: 1rem; padding: 1rem;'>
-      <h4>${offer.title}</h4>
-      <p>${offer.where}</p>
-      <p>${offer.date}</p>
-      <p>${offer.price}€</p>
-      <a href='${offer.link}'>${offer.link}</a>
-    </div>
-  `).join('')
+const filterNewOffers = offers => offers.filter(offer => !Object.keys(OFFERS).includes(offer.id))
+
+const telegramHandler = offers => {
+  if (offers.length === 0) {
+    console.log('No new offers at the moment');
+  }
+  else if (offers.length === 1) {
+    const o = offers[0]
+    Bot.sendMessage(chatId, `
+      New offer ${o.title}
+      Price: ${o.price}
+      Where: ${o.where}
+      ${o.link}
+    `);
+  } else {
+    Bot.sendMessage(chatId, `${offers.length} new offers, go to https://www.leboncoin.fr/mes-recherches`);
+  }
+
+  return offers
 }
 
-const urls = config.scrapper && config.scrapper.urls
+const mailHandler = offers => {
 
-const run = () => {
-  const now = moment().format('MMMM Do YYYY, h:mm:ss a')
-  console.log(`${now} - Recovering Offers for urls: ${urls.join(' & ')}`)
+  const prepareMailHtml = offers => {
+    return offers.map(offer => `
+      <div className='offer' style='background-color: gold; margin: 1rem; padding: 1rem;'>
+        <h4>${offer.title}</h4>
+        <p>${offer.where}</p>
+        <p>${offer.date}</p>
+        <p>${offer.price}€</p>
+        <a href='${offer.link}'>${offer.link}</a>
+      </div>
+    `).join('')
+  }
 
-  return Promise.all(urls.map(recoverOffers))
-  .then(flatMap)
-  .then(offers => {
-    const newOffers = offers.filter(offer => !Object.keys(OFFERS).includes(offer.id))
-
-    if (newOffers.length === 0) { console.log('none'); return }
-
-    newOffers.forEach(offer => OFFERS[offer.id] = offer)
-
-    fs.writeFile(file, JSON.stringify(OFFERS, null, 2), err => {
-      if (err) {
-        console.error("Failed to saved offers to %s: %s", file, err)
-      } else {
-        console.log(`${newOffers.length} new offers saved to %s`, file)
-      }
-    })
-
-    return Mail(config.mail).send({
-      to: config.mail.to.join(', '),
-      subject: "[LBC-Watcher] New offers",
-      text: "",
-      html: prepareMailHtml(newOffers)
-    })
+  Mail(config.mail).send({
+    to: config.mail.to.join(', '),
+    subject: "[LBC-Watcher] New offers",
+    text: "",
+    html: prepareMailHtml(offers)
   })
+
+  return offers
 }
 
-setInterval(run, config.scrapper.delay * 60 * 1000)
-run().then(() => console.log('all good'), console.error)
+const save = offers => {
+  offers.forEach(offer => OFFERS[offer.id] = offer)
+
+  fs.writeFile(file, JSON.stringify(OFFERS, null, 2), err => {
+    if (err) {
+      console.error("Failed to saved offers to %s: %s", file, err)
+    } else {
+      console.log(`${offers.length} new offers saved to %s`, file)
+    }
+  })
+
+  return offers
+}
+
+const loop = offers => {
+  Scrapper.end()
+
+  // add some random delay (between 0 and 10 seconds)
+  const randomSeconds = Math.random() * 10
+  const ms = (config.scrapper.delay * 60 + randomSeconds) * 1000
+  console.log(`next run in ${ms / 1000} seconds`)
+  setTimeout(run, ms)
+
+  return offers
+}
+
+const run = async () => {
+  await Scrapper.init()
+
+  return Promise.all(urls.map(Scrapper.recoverOffers))
+    .then(flatMap)
+    .then(filterNewOffers)
+    .then(telegramHandler)
+    .then(save)
+    .then(loop)
+}
+
+run()
