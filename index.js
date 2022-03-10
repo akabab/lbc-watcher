f13138d9ed609a2dd9d64dc67548d25de091e4f0
Yoann Cribier <yoann_cribier@hotmail.com>
2022-03-10 12:40:55 +0400

    All this is way more cleaner ...

--- index.js
+++ index.js
@@ -45,18 +45,215 @@ const ENV = {
   CHROME_WINDOW_SIZE: process.env.CHROME_WINDOW_SIZE || '1920,1080',
   WATCHER_DUMP_FILE_PATH: process.env.WATCHER_DUMP_FILE_PATH || path.join(__dirname, 'dump.json'),
   WATCHER_DEFAULT_DELAY_IN_SECONDS: Number(process.env.WATCHER_DEFAULT_DELAY_IN_SECONDS),
-  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  TELEGRAM_BOT_POLLING_OFFSET: Number(process.env.TELEGRAM_BOT_POLLING_OFFSET)
 }
 
 // == GLOBALS == //
 let G_BROWSER
 
-let G_DUMP = {}
+let G_BOT
+let G_IAMROBOT = 0
 
-const G_ACTIVE_PAGES = {}
+const G_WATCHERS = {}
+let G_WATCHERS_PID = 0
+
+
+// == TELEGRAM == //
+const setupBot = Bot => {
+
+  Bot.on('message', msg => { console.log('BOT MESSAGE:', msg, { Bot }) })
+
+  Bot.on('polling_error', error => { console.log(error.code) })
+
+  // /id
+  Bot.onText(/^\/id$/, msg => { Bot.sendMessage(msg.chat.id, `Chat ID: ${msg.chat.id}`) })
+
+  // /list | /ls (alias)
+  Bot.onText(/^(\/list|\/ls)$/, msg => {
+    const chatId = msg.chat.id
+
+    const format = w => `${w.pid} [url](${w.search.url}) ${w.search.delay}s ${w.search.active ? 'active' : 'stopped'} `
+
+    const thisChatWatchers = Object.values(G_WATCHERS).filter(w => w.search.chatId === chatId)
+
+    const message = thisChatWatchers.length > 0
+      ? thisChatWatchers.map(format).join('\n')
+      : 'NO_CURRENT_WATCHERS'
+
+    Bot.sendMessage(chatId, message, { parse_mode: 'MarkdownV2' })
+  })
+
+  // /new <url> <delay?>
+  Bot.onText(/^\/new (.+)/, (msg, match) => {
+    const chatId = msg.chat.id
+    const args = match[1].split(' ')
+
+    const url = args[0]
+    const delay = Number(args[1]) || 300 // TODO: check for range min max and isInteger
+
+    if (!url.startsWith('https://www.leboncoin.fr/recherche?')) {
+      Bot.sendMessage(chatId, 'INVALID_FORMAT /new <URL> <?DELAY_IN_SECONDS>')
+      return
+    }
+
+    const id = `${chatId}-${url}`
+
+    const search = {
+      id,
+      chatId,
+      url,
+      delay,
+      'active': true,
+      'lastSearchDate': "2000-01-01T00:00:00.000Z"
+    }
+
+    const watcher = {
+      pid: G_WATCHERS_PID++,
+      search,
+    }
+
+    G_WATCHERS[watcher.pid] = watcher
+
+    persistDumpFile()
+
+    startWatcher(watcher)
+
+    Bot.sendMessage(chatId, 'NEW_SUCCESS')
+  })
+
+  // /setdelay <pid> <delay>
+  Bot.onText(/^\/setdelay (\d+) (\d+)/, (msg, match) => {
+    const chatId = msg.chat.id
+    const pid = Number(match[1])
+    const delay = Number(match[2])
+    const watcher = G_WATCHERS[pid]
+
+    if (!watcher) {
+      Bot.sendMessage(chatId, `INVALID PID ${pid}`)
+      return
+    }
+
+    if (delay < 60) {
+      Bot.sendMessage(chatId, `INVALID_DELAY: [60-99999]`)
+      return
+    }
+
+    G_WATCHERS[pid].search.delay = delay
+
+    persistDumpFile()
+
+    Bot.sendMessage(chatId, 'SETDELAY_SUCCESS')
+  })
+
+  // /stop <pid>
+  Bot.onText(/^\/stop (\d+)$/, (msg, match) => {
+    const chatId = msg.chat.id
+    const pid = Number(match[1])
+    const watcher = G_WATCHERS[pid]
+
+    if (!watcher) {
+      Bot.sendMessage(chatId, `INVALID PID ${pid}`)
+      return
+    }
+
+    watcher.search.active = false
+
+    persistDumpFile()
+
+    Bot.sendMessage(chatId, 'STOP_SUCCESS')
+  })
+
+  // /start <pid>
+  Bot.onText(/^\/start (\d+)$/, (msg, match) => {
+    const chatId = msg.chat.id
+    const pid = Number(match[1])
+    const watcher = G_WATCHERS[pid]
+
+    if (!watcher) {
+      Bot.sendMessage(chatId, `INVALID PID ${pid}`)
+      return
+    }
+
+    watcher.search.active = true
+
+    persistDumpFile()
+
+    startWatcher(watcher)
+
+    Bot.sendMessage(chatId, 'START_SUCCESS')
+  })
+
+  // /del <pid>
+  Bot.onText(/^\/del (\d+)$/, (msg, match) => {
+    const chatId = msg.chat.id
+    const pid = Number(match[1])
+
+    // define inline keyboard to send to user
+    const optionalParams = {
+      parse_mode: 'Markdown',
+      reply_markup: JSON.stringify({
+        inline_keyboard: [[
+          { text: 'Yes', callback_data: `DEL ${pid}` },
+          { text: 'No', callback_data: 'good' }
+        ]
+        ]
+      })
+    }
+
+    // reply when user sends a message, and send him our inline keyboard as well
+    Bot.sendMessage(chatId, 'Are you sure ?', optionalParams);
+  })
+
+  // Because each inline keyboard button has callback data, you can listen for the callback data and do something with them
+  Bot.on('callback_query', query => {
+    // console.log({query})
+
+    // REMOVE KEYBOARD
+    const optionalParams = {
+      parse_mode: 'Markdown',
+      reply_markup: JSON.stringify({
+        remove_keyboard: true,
+        selective: true
+      })
+    }
+
+    if (query.data.startsWith('DEL ')) {
+      const pid = Number(query.data.split(' ')[1])
+
+      // FLAG Watcher to get deleted during watch loop
+      G_WATCHERS[pid].SHOULD_BE_DELETED = true
+
+      Bot.answerCallbackQuery(query.id, {
+        text: `[${pid}] Successfully deleted`,
+        // show_alert: true
+      })
+
+      // Bot.editMessageReplyMarkup(query.message.chat.id,)
+
+      Bot.sendMessage(query.message.chat.id, `[${pid}] Successfully deleted`, optionalParams)
+    }
+
+    if (query.data === 'hello') {
+      Bot.sendMessage(query.message.chat.id, 'Hello to you too!')
+    }
+
+
+    Bot.sendMessage(query.message.chat.id, '', optionalParams)
+  })
+
+  // /seppuku : kill program
+  Bot.onText(/^\/seppuku$/, async msg => {
+    const chatId = msg.chat.id
+    console.log("SEPPUKUUUUU")
+
+    await Bot.sendMessage(chatId, `Sayonara !`)
+
+    // process.exit(1)
+  })
+}
 
 // == WS ENDPOINT == //
-
 /**
 * Poll browser.log periodically until we see the wsEndpoint
 * that we use to connect to the browser.
@@ -91,7 +288,11 @@ const persistDumpFile = async () => {
 
   console.log('Persisting dump file...', filePath)
   try {
-    /* await */ fsp.writeFile(filePath, JSON.stringify(G_DUMP, null, 2))
+    const content = Object.values(G_WATCHERS).map(w => w.search)
+
+    console.log({G_WATCHERS, content})
+
+    /* await */ fsp.writeFile(filePath, JSON.stringify(content, null, 2))
     console.log('Dump file successfully saved', filePath)
   } catch (err) {
     console.error('Failed to save to dump file', filePath, err)
@@ -104,16 +305,18 @@ const loadDumpFile = async () => {
   console.log('Loading dump file...', filePath)
   try {
     const content = await fsp.readFile(filePath)
-    G_DUMP = JSON.parse(content)
+    const jsonContent = JSON.parse(content)
+
     console.log('Dump file successfully loaded', filePath)
+
+    return jsonContent
   } catch (err) {
-    /* ignore file missing ENOENT and continue */
-    console.log('No dump file found', filePath, err)
+    /* ignore file missing ENOENT or empty and continue */
+    console.error('Dump file', err)
   }
 }
 
 // == SEARCH == //
-
 const parseOffer = offer => ({
   id: String(offer.list_id),
   date: `${offer.index_date} GMT+0100`,
@@ -125,7 +328,9 @@ const parseOffer = offer => ({
   image: offer.images.thumb_url
 })
 
-const cookiesHandler = async (search, page) => {
+const cookiesHandler = async watcher => {
+  const { search, page } = watcher
+
   try {
     // Continuer sans accepter
     // await page.humanclick('button#didomi-notice-disagree-button').click()
@@ -149,12 +354,20 @@ const cookiesHandler = async (search, page) => {
   }
 }
 
-const datadomeHandler = async (search, page) => {
+const datadomeHandler = async watcher => {
+  const { search, page } = watcher
 
   try {
     await page.waitForSelector('meta[content^="https://img.datadome.co/captcha"', { timeout: 10000 })
     console.log(`[${search.id}] I am a robot =(`)
-    Bot.sendMessage(search.chatId, 'I am a robot =(')
+    G_IAMROBOT++
+
+    if (G_IAMROBOT > 10) {
+      G_BOT.sendMessage(search.chatId, 'Leaving this world... of humans!')
+      process.exit(1)
+    }
+
+    G_BOT.sendMessage(search.chatId, 'I am a robot =(')
     return true
   } catch {
     console.log(`[${search.id}] I am human...`)
@@ -163,7 +376,9 @@ const datadomeHandler = async (search, page) => {
   return false
 }
 
-const debugHandler = async (search, page) => {
+const debugHandler = async watcher => {
+  const { search, page } = watcher
+
   page.on('request', async r => {
     if (search.url === r._initializer.url) {
       console.log(search.id, 'REQUEST', { headers: await r.allHeaders() })
@@ -177,7 +392,25 @@ const debugHandler = async (search, page) => {
   })
 }
 
-const searchHandler = async (search, page) => {
+const watcherHandler = async watcher => {
+  const { search, page } = watcher
+
+  if (watcher.SHOULD_BE_DELETED) {
+    console.log(`[${search.id}] SHOULD_BE_DELETED, Aborting watcher...`)
+    page.close()
+    delete G_WATCHERS[watcher.pid]
+    persistDumpFile()
+    return
+  }
+
+  // ABORT if search is not active
+  if (!search.active) {
+    console.log(`[${search.id}] STOPPED, Closing page...`)
+    delete watcher.page
+    page.close()
+    return
+  }
+
   console.log(`[${search.id}] New search...`)
 
   await page.reload()
@@ -186,11 +419,11 @@ const searchHandler = async (search, page) => {
   // await page.screenshot({ path: screenFilePath })
 
   // Datadome
-  const iambot = await datadomeHandler(search, page)
+  const iambot = await datadomeHandler(watcher)
 
   if (!iambot) {
     // Cookies
-    await cookiesHandler(search, page)
+    await cookiesHandler(watcher)
 
     await page.waitForSelector('script#__NEXT_DATA__')
     const datas = await page.evaluate(() => document.querySelector('script#__NEXT_DATA__').innerHTML)
@@ -216,7 +449,7 @@ const searchHandler = async (search, page) => {
     if (newOffers.length > 0 && newOffers.length < 5) {
 
       newOffers.map(o => {
-        Bot.sendMessage(search.chatId, `
+        G_BOT.sendMessage(search.chatId, `
           New offer ${o.title}
           Date: ${o.date}
           Price: ${o.price} €
@@ -225,7 +458,7 @@ const searchHandler = async (search, page) => {
         `)
       })
     } else if (newOffers.length >= 5) {
-      Bot.sendMessage(search.chatId, `${newOffers.length} new offers, go to ${search.url}`)
+      G_BOT.sendMessage(search.chatId, `${newOffers.length} new offers, go to ${search.url}`)
     }
   }
 
@@ -236,12 +469,12 @@ const searchHandler = async (search, page) => {
   console.log(`[${search.id}] Next search in ${ms / 1000} seconds`)
   await wait(ms)
 
-  searchHandler(search, page)
+  watcherHandler(watcher)
 }
 
-const startSearchWatcher = async search => {
+const startWatcher = async watcher => {
   // OPEN PAGE (once)
-  console.log(`[${search.id}] New page`)
+  console.log(`[${watcher.search.id}] New page`)
   const page = await G_BROWSER.newPage()
   // await page.setExtraHTTPHeaders({ 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:91.0) Gecko/20100101 Firefox/91.0' })
 
@@ -250,16 +483,16 @@ const startSearchWatcher = async search => {
 
   await installMouseHelper(page)
 
-  if (process.env.DEBUG) { debugHandler(search, page) }
+  if (process.env.DEBUG) { debugHandler(watcher) }
 
-  await page.goto(search.url)
+  await page.goto(watcher.search.url)
 
-  G_ACTIVE_PAGES[search.id] = page
+  watcher.page = page
 
-  searchHandler(search, page)
+  watcherHandler(watcher)
 }
 
-const debugbotHandler = () => {
+const debugbotHandler = async () => {
   // await page.goto('https://antoinevastel.com/bots/')
   // await page.waitForTimeout(5000)
   // await page.screenshot({ path: 'fp.png', fullPage: true })
@@ -278,8 +511,6 @@ const debugbotHandler = () => {
 }
 
 const main = async () => {
-  await loadDumpFile()
-
   // LAUNCH A BROWSER (ONLY 1 IS NECESSARY)
   const command = ENV.CHROME_BINARY
     + (ENV.CHROME_IS_HEADLESS ? ' --headless': '')
@@ -293,16 +524,6 @@ const main = async () => {
 
   console.log('Launching browser...', command)
   const browserProcess = exec(command, (error, stdout, stderr) => console.log({error, stdout, stderr}))
-//
-//   process.on('exit', async () => {
-//     console.log(`EXIT`)
-//
-//     await setTimeout(() => {
-//       console.log(`EXIT, killing browser process pid: ${browserProcess.pid}`)
-//       browserProcess.kill('SIGHUP')
-//     }, 5000)
-//     // exec(`pkill -f "remote-debugging-port=92"`)
-//   })
 
   const browserWSEndpoint = await getBrowserWSEndpoint()
 
@@ -321,74 +542,29 @@ const main = async () => {
   // DEBUG BOT DETECTION
   if (ENV.DEBUG_BOT) { await debugbotHandler() }
 
+  // INIT BOT
+  const pollingOffset = -10 //ENV.TELEGRAM_BOT_POLLING_OFFSET
+  const polling = pollingOffset ? { params: { offset: pollingOffset } } : true
+  G_BOT = new TelegramBot(ENV.TELEGRAM_BOT_TOKEN, { polling })
+  setupBot(G_BOT)
+
   // START WATCHERS FOR ALL SEARCHS
-  const searchs = Object.values(G_DUMP)
+  const searchs = (await loadDumpFile()) || []
+
   for (let search of searchs) {
-    startSearchWatcher(search)
-    await wait(10000)
+    if (search.active) {
+      const watcher = {
+        pid: G_WATCHERS_PID++,
+        search,
+      }
+
+      G_WATCHERS[watcher.pid] = watcher
+
+      startWatcher(watcher)
+
+      await wait(5000)
+    }
   }
 }
-
-// == TELEGRAM == //
-const Bot = new TelegramBot(ENV.TELEGRAM_BOT_TOKEN, { polling: true })
-
-Bot.onText(/^(\/list|\/ls)$/, msg => {
-  const chatId = msg.chat.id
-
-  const formatSearchText = s => {
-    const searchTextMatch = s.url.match(/text=(.*)&loc/)
-    return searchTextMatch && searchTextMatch.length > 1 ? searchTextMatch[1] : '???'
-  }
-  const formatSearch = s => `| ${s.id} | [${formatSearchText(s)}](${s.url}) | ${s.delay}s | ${s.active ? 'yes' : 'no'} |`
-
-  const searchs = Object.values(G_DUMP).filter(s => s.chatId === chatId)
-
-  const message = searchs.map(formatSearch).join('\n')
-
-  Bot.sendMessage(chatId, message)
-})
-
-// stop <pid>
-// delete del <pid>
-// setdelay <pid> <delay>
-
-
-Bot.onText(/^\/new (.+)/, (msg, match) => {
-  const chatId = msg.chat.id
-  const command = match[1].split(' ')
-
-  const url = command[0]
-  const delay = Number(command[1]) || 300 // TODO: check for range min max and isInteger
-
-  if (!url.startsWith('https://www.leboncoin.fr/recherche?')) {
-    Bot.sendMessage(chatId, 'INVALID_FORMAT /new <URL> <?DELAY_IN_SECONDS>')
-    return
-  }
-
-  const id = `${chatId}-${url}`
-
-  const newSearch = {
-    id,
-    chatId,
-    url,
-    delay,
-    'active': true,
-    'lastSearchDate': "2000-01-01T00:00:00.000Z"
-  }
-
-  G_DUMP[id] = newSearch
-
-  persistDumpFile()
-
-  startSearchWatcher(newSearch)
-
-  Bot.sendMessage(chatId, 'NEW_SUCCESS')
-})
-
-Bot.onText(/^\/id$/, msg => {
-  const chatId = msg.chat.id
-
-  Bot.sendMessage(chatId, `Chat ID: ${chatId}`)
-})
 
 main()
